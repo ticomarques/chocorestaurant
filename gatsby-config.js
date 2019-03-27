4af35c5d47edb9147e51ff55204a51584c5d80cc
Tiago Marques <thymarques@gmail.com>
2019-03-27 11:06:51 -0700

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Choco Restaurant`,
+    description: `A new project for Choco Restaurant using Gatsby.`,
+    author: `Tiago Marques`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
